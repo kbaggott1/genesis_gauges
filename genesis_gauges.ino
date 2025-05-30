de6ef8aa99f0d9bada303dc90f97e72666c0bb0e
kbaggott1 <93663166+kbaggott1@users.noreply.github.com>
2025-05-30 15:23:11 -0400

    Update code

--- genesis_gauges.ino
+++ genesis_gauges.ino
@@ -7,10 +7,8 @@
 // SETTINGS =========================================================================================
 
 // The voltage reading from the pressure sensor to activate boost mode on the gauges.
-const float VOLTAGE_THRESHOLD_UPPER = 1.2;
-
-// The voltage reading from the pressure sensor to deactivate boost mode on the gauges.
-const float VOLTAGE_THRESHOLD_LOWER = 0.8;
+const float VOLTAGE_ACTIVATE_BOOST_MODE = 1.2;
+const float VOLTAGE_DEACTIVATE_BOOST_MODE = 0.8;
 
 // The time in seconds the gauges will remain in boost mode AFTER the lower voltage threshold was hit.
 const float GAUGE_DELAY_IN_S = 1.0;
@@ -19,80 +17,78 @@ const float GAUGE_DELAY_IN_S = 1.0;
 const char NUM_FLASHES = 2;
 
 // The time in milliseconds a color will be held in flash mode.
-// AKA Flash speed.
 const int FLASH_DELAY_IN_MS = 100;
 
 // ==================================================================================================
 
-float voltage_threshold = VOLTAGE_THRESHOLD_UPPER;
+float voltage_threshold = VOLTAGE_ACTIVATE_BOOST_MODE;
 bool has_flashed = false;
 bool default_on_blue = true;
 unsigned long previous_time = 0;
+bool is_red = false; 
 
+// Helper Functions =================================================================================
+
+void set_color(bool red) {
+  if (is_red != red) {
+    if (default_on_blue) {
+      digitalWrite(RELAY_PIN, red ? HIGH : LOW);
+    } else {
+      digitalWrite(RELAY_PIN, red ? LOW : HIGH);
+    }
+    is_red = red;
+  }
+}
 
 void turn_blue() {
-  if(default_on_blue)
-    digitalWrite(RELAY_PIN, LOW);
-  else
-    digitalWrite(RELAY_PIN, HIGH);
+  set_color(false);
 }
 
 void turn_red() {
-  if(default_on_blue)
-    digitalWrite(RELAY_PIN, HIGH);
-  else
-    digitalWrite(RELAY_PIN, LOW);
+  set_color(true);
+}
+
+void flash() {
+  for (char i = 0; i < NUM_FLASHES; i++) {
+    turn_red();
+    delay(FLASH_DELAY_IN_MS);
+    turn_blue();
+    delay(FLASH_DELAY_IN_MS);
+  }
 }
 
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(SWITCH_DEFAULT_COLOR_PIN, INPUT_PULLUP);
 
-  int switch_state = digitalRead(SWITCH_DEFAULT_COLOR_PIN);
-  default_on_blue = (switch_state == HIGH);
+  default_on_blue = (digitalRead(SWITCH_DEFAULT_COLOR_PIN) == HIGH);
 
   turn_blue();
   Serial.begin(9600);
-
-}
-
-void flash() {
-  turn_red();
-
-  for(char i = 0; i < NUM_FLASHES; i++) {
-    turn_blue();
-    delay(FLASH_DELAY_IN_MS);
-    turn_red();
-    delay(FLASH_DELAY_IN_MS);
-  }
 }
 
 void loop() {
   int analog_value = analogRead(TURBO_PIN);
   float voltage = analog_value * (5.0 / 1023.0);
-
-  int switch_state = digitalRead(SWITCH_DEFAULT_COLOR_PIN);
-  default_on_blue = (switch_state == HIGH);
-
   Serial.println(voltage);
 
-  if(voltage > voltage_threshold) {
-    if(!has_flashed) {
+  if (voltage > voltage_threshold) {
+    if (!has_flashed) {
       flash();
-      voltage_threshold = VOLTAGE_THRESHOLD_LOWER;
+      voltage_threshold = VOLTAGE_DEACTIVATE_BOOST_MODE;
       has_flashed = true;
     }
     turn_red();
     previous_time = millis();
   } 
   else {
-    if((millis() - previous_time) <= GAUGE_DELAY_IN_S * 1000) {
+    if ((millis() - previous_time) <= GAUGE_DELAY_IN_S * 1000) {
       turn_red();
-    }
+    } 
     else {
       turn_blue();
-      voltage_threshold = VOLTAGE_THRESHOLD_UPPER;
-      has_flashed = false;      
+      voltage_threshold = VOLTAGE_ACTIVATE_BOOST_MODE;
+      has_flashed = false;
     }
   }
 }
